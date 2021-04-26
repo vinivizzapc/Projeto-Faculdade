25d10b196f4b09167583543b116f286a801435a0
Vitorgpc <vitorcolazelli@gmail.com>
2021-04-26 01:44:49 -0300

    subindo projeto certo

--- frontend/App.js
+++ frontend/App.js
@@ -1,60 +1,36 @@
-import React from 'react';
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-import { StyleSheet, Text, View, SafeAreaView, ScrollView, Dimensions, Image } from 'react-native';
-import { createDrawerNavigator, createStackNavigator, DrawerItems } from 'react-navigation';
-import HomeScreen from './screens/HomeScreen';
-import FavoritosScreen from './screens/FavoritosScreen';
-import MapsScreen from './screens/MapsScreen';
-import AgendaConsultaScreen from './screens/AgendaConsultaScreen';
-import MinhasConsultaScreen from './screens/MinhasConsultaScreen';
-import LoginScreen from './screens/LoginScreen';
-import CadastroScreen from './screens/CadastroScreen';
-
-
-const {width} = Dimensions.get('window');
-export default class App extends React.Component{
-
-  render() {
+function HomeScreen({ navigation }) {
   return (
-      <AppDrawerNavigator />
-    );
-  }
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
 }
 
-const CustomDrawerComponent = (props) => (
-  <SafeAreaView style={{ flex: 1 }}>
-    <View style={{height:150, backgroundColor:'white', alignItems:'center', justifyContent:'center'}}>
-      <Image source={require('./assets/ftperfil.png')} style={{height:80,width:80,borderRadius:60, }}/>
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
     </View>
-    <ScrollView>
-      <DrawerItems {...props}/>
-    </ScrollView>
-  </SafeAreaView>
-)
+  );
+}
 
-const AppDrawerNavigator = createDrawerNavigator({
-  Home: HomeScreen,
-  Mapa: MapsScreen,
-  'Agende sua consulta': AgendaConsultaScreen,
-  'Minhas consultas': MinhasConsultaScreen,
-  Favoritos: FavoritosScreen,
-  Login: LoginScreen,
-  Cadastro: CadastroScreen,
-},{
-    contentComponent: CustomDrawerComponent,
-    contentOptions: {
-      activeTintColor: 'orange'
-    }
-  })
+const Drawer = createDrawerNavigator();
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-console.disableYellowBox = true;
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
