715c72ee1ff88e4a74054649555c9e92a85eea6b
Vitorgpc <vitorcolazelli@gmail.com>
2021-05-01 19:16:08 -0300

    Diferenciando login de adm e de usuario

--- frontend/components/DrawerContent.js
+++ frontend/components/DrawerContent.js
@@ -1,6 +1,6 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StyleSheet } from 'react-native';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HomeScreen from '../screens/HomeScreen';
 import PrevencoesScreen from '../screens/PrevencoesScreen';
 import MapsScreen from '../screens/MapsScreen';
@@ -9,29 +9,41 @@ import AgendaConsultaScreen from '../screens/AgendaConsultaScreen';
 import FavoritosScreen from '../screens/FavoritosScreen';
 import { Ionicons, Feather, FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
 import CustomDrawerContent from './CustomDrawerContent';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Drawer = createDrawerNavigator();
 
 function DrawerContent() {
+  const [status , setStatus] = useState(0);
+
+  useEffect(() => {
+    async function CheckUser(){
+      const usuario = await AsyncStorage.getItem('user');
+      const jsonValue = JSON.parse(usuario);
+      setStatus(jsonValue.status);
+    }
+
+    CheckUser();
+  }, []);
+
   return (
     <Drawer.Navigator drawerContent={props=><CustomDrawerContent {...props}/>} initialRouteName="Home">
-        <Drawer.Screen options={{ title: 'Home',  drawerIcon: ({focused, size}) => (<Ionicons name="home" size={24} color="black" />
-          ),}} name="Home" component={HomeScreen} />
-
-        <Drawer.Screen options={{ title: 'Prevenções',  drawerIcon: ({focused, size}) => (<FontAwesome5 name="shield-virus" size={24} color="black" />
-          ),}} name="Prevenções" component={PrevencoesScreen} />
-
-        <Drawer.Screen options={{ title: 'Localizar',  drawerIcon: ({focused, size}) => (<Feather name="map-pin" size={24} color="black" />
-          ),}} name="Localizar" component={MapsScreen} />
-
-        <Drawer.Screen options={{ title: 'Minhas Consultas',  drawerIcon: ({focused, size}) => (<MaterialCommunityIcons name="clipboard-pulse-outline" size={24} color="black" />
-          ),}} name="Minhas Consultas" component={MinhasConsultasScreen} />
-
-        <Drawer.Screen options={{ title: 'Agendar Consultas',  drawerIcon: ({focused, size}) => (<FontAwesome5 name="calendar-alt" size={24} color="black" />
-          ),}} name="Agendar Consultas" component={AgendaConsultaScreen} />
-
-        <Drawer.Screen options={{ title: 'Favoritos',  drawerIcon: ({focused, size}) => (<Ionicons name="star" size={24} color="black" />
-          ),}} name="Favoritos" component={FavoritosScreen} />
+      {
+        status == 0 ? (
+          <>
+            <Drawer.Screen options={{ title: 'Home',  drawerIcon: ({focused, size}) => (<Ionicons name="home" size={24} color="black" />),}} name="Home" component={HomeScreen} />
+            <Drawer.Screen options={{ title: 'Prevenções',  drawerIcon: ({focused, size}) => (<FontAwesome5 name="shield-virus" size={24} color="black" />),}} name="Prevenções" component={PrevencoesScreen} />
+            <Drawer.Screen options={{ title: 'Localizar',  drawerIcon: ({focused, size}) => (<Feather name="map-pin" size={24} color="black" />),}} name="Localizar" component={MapsScreen} />
+            <Drawer.Screen options={{ title: 'Minhas Consultas',  drawerIcon: ({focused, size}) => (<MaterialCommunityIcons name="clipboard-pulse-outline" size={24} color="black" />),}} name="Minhas Consultas" component={MinhasConsultasScreen} />
+            <Drawer.Screen options={{ title: 'Agendar Consultas',  drawerIcon: ({focused, size}) => (<FontAwesome5 name="calendar-alt" size={24} color="black" />),}} name="Agendar Consultas" component={AgendaConsultaScreen} />
+            <Drawer.Screen options={{ title: 'Favoritos',  drawerIcon: ({focused, size}) => (<Ionicons name="star" size={24} color="black" />),}} name="Favoritos" component={FavoritosScreen} />
+          </>
+        )
+        :
+        <>
+          <Drawer.Screen options={{ title: 'Favoritos',  drawerIcon: ({focused, size}) => (<Ionicons name="star" size={24} color="black" />),}} name="Favoritos" component={FavoritosScreen} />
+        </>
+      }
       </Drawer.Navigator>
   );
 }
